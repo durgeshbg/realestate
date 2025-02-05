d1b82c263c0d7a1dba690a51fb438204ccbbd441
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2025-02-05 08:36:04 +0530

    add: message_template migration

---
+++ database/migrations/2025_02_05_025942_create_message_templates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('message_templates', function (Blueprint $table) {
+            $table->id();
+            $table->enum('workflow_id', ['Cold', 'Follow up', 'Warm', 'Hot', 'Won', 'Lost', 'No contact']);
+            $table->text('template');
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('deleted_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('deleted_by')->references('id')->on('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('message_templates');
+    }
+};
